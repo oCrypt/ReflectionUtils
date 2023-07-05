0a3c3f6dfdafa816435f1d661b84c3d308f33347
ocrypt <universeplaysvg@gmail.com>
2023-07-05 12:49:00 -0400

    add more stuff to utility

--- src/main/java/com/cahrypt/reflectionutils/ReflectionUtils.java
+++ src/main/java/com/cahrypt/reflectionutils/ReflectionUtils.java
@@ -1,24 +1,218 @@
 package com.cahrypt.reflectionutils;
 
 import java.io.File;
+import java.lang.reflect.AccessibleObject;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Member;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public final class ReflectionUtils {
 
     /**
-     * Checks whether a field is static and final.
-     * @param field The field to check.
-     * @return Whether the field is static and final.
+     * Get all the fields of a class matching a predicate.
+     * @param clazz The class to get the fields from.
+     * @param predicate The predicate to match the fields against.
+     * @return A set of fields matching the predicate.
      */
-    public static boolean isStaticFinal(Field field) {
-        return field.toGenericString().contains("static final");
+    public static Set<Field> getAllFields(Class<?> clazz, Predicate<Field> predicate) {
+        return Arrays.stream(clazz.getDeclaredFields())
+                .filter(predicate)
+                .collect(Collectors.toSet());
+    }
+
+    /**
+     * Get all the methods of a class matching a predicate.
+     * @param clazz The class to get the methods from.
+     * @param predicate The predicate to match the methods against.
+     * @return A set of methods matching the predicate.
+     */
+    public static Set<Method> getAllMethods(Class<?> clazz, Predicate<Method> predicate) {
+        return Arrays.stream(clazz.getDeclaredMethods())
+                .filter(predicate)
+                .collect(Collectors.toSet());
+    }
+
+    /**
+     * Get all the constructors of a class matching a predicate.
+     * @param clazz The class to get the constructors from.
+     * @param predicate The predicate to match the constructors against.
+     * @return A set of constructors matching the predicate.
+     */
+    public static Set<Constructor<?>> getAllConstructors(Class<?> clazz, Predicate<Constructor<?>> predicate) {
+        return Arrays.stream(clazz.getDeclaredConstructors())
+                .filter(predicate)
+                .collect(Collectors.toSet());
+    }
+
+    /**
+     * Get all the fields of a class.
+     * @param clazz The class to get the fields from.
+     * @return A set of fields.
+     */
+    public static Set<Field> getAllFields(Class<?> clazz) {
+        return getAllFields(clazz, field -> true);
+    }
+
+    /**
+     * Get all the methods of a class.
+     * @param clazz The class to get the methods from.
+     * @return A set of methods.
+     */
+    public static Set<Method> getAllMethods(Class<?> clazz) {
+        return getAllMethods(clazz, method -> true);
+    }
+
+    /**
+     * Get all the constructors of a class.
+     * @param clazz The class to get the constructors from.
+     * @return A set of constructors.
+     */
+    public static Set<Constructor<?>> getAllConstructors(Class<?> clazz) {
+        return getAllConstructors(clazz, constructor -> true);
+    }
+
+    /**
+     * Consume a field which is ensured to be accessible to the accessor.
+     * @param obj The object to consume.
+     * @param accessor The object accessing the field.
+     * @param consumer The consumer.
+     */
+    public static <T extends AccessibleObject> void consumeAccessibleObject(T obj, Object accessor, Consumer<T> consumer) {
+        boolean accessible = obj.canAccess(accessor);
+
+        if (!accessible) {
+            obj.setAccessible(true);
+        }
+
+        consumer.accept(obj);
+
+        if (!accessible) {
+            obj.setAccessible(false);
+        }
+    }
+
+    /**
+     * Consume a field which is ensured to be accessible to the accessor.
+     * @param fieldName The field name.
+     * @param clazz The class where the field is situated.
+     * @param accessor The object accessing the field.
+     * @param consumer The consumer.
+     */
+    public static void consumeAccessibleField(String fieldName, Class<?> clazz, Object accessor, Consumer<Field> consumer) {
+        Field field;
+
+        try {
+            field = clazz.getField(fieldName);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        consumeAccessibleObject(field, accessor, consumer);
+    }
+
+    /**
+     * Checks whether a member is static and final.
+     * @param member The member to check.
+     * @return Whether the member is static and final.
+     */
+    public static boolean isStaticFinal(Member member) {
+        return isStatic(member) && isFinal(member);
+    }
+
+    /**
+     * Checks whether a member is public.
+     * @param member The member to check.
+     * @return Whether the member is public.
+     */
+    public static boolean isPublic(Member member) {
+        return hasModifier(member, Modifier.PUBLIC);
+    }
+
+    /**
+     * Checks whether a member is protected.
+     * @param member The member to check.
+     * @return Whether the member is protected.
+     */
+    public static boolean isProtected(Member member) {
+        return hasModifier(member, Modifier.PROTECTED);
+    }
+
+    /**
+     * Checks whether a member is private.
+     * @param member The member to check.
+     * @return Whether the member is private.
+     */
+    public static boolean isPrivate(Member member) {
+        return hasModifier(member, Modifier.PRIVATE);
+    }
+
+    /**
+     * Checks whether a member is static.
+     * @param member The member to check.
+     * @return Whether the member is static.
+     */
+    public static boolean isStatic(Member member) {
+        return hasModifier(member, Modifier.STATIC);
+    }
+
+    /**
+     * Checks whether a member is final.
+     * @param member The member to check.
+     * @return Whether the member is final.
+     */
+    public static boolean isFinal(Member member) {
+        return hasModifier(member, Modifier.FINAL);
+    }
+
+    /**
+     * Checks whether a member has a specific name prefix.
+     * @param member The member to check.
+     * @param prefix The prefix to check for.
+     * @return Whether the member has the prefix.
+     */
+    public static boolean hasPrefix(String prefix, Member member) {
+        return member.getName().startsWith(prefix);
+    }
+
+    /**
+     * Checks whether a member has a specific modifier.
+     * @param member The member to check.
+     * @param modifier The modifier to check for (in string form).
+     * @return Whether the member has the modifier.
+     */
+    public static boolean hasModifier(Member member, String modifier) {
+        return getModifierString(member).contains(modifier);
+    }
+
+    /**
+     * Checks whether a member has a specific modifier.
+     * @param member The member to check.
+     * @param modifier The modifier to check for (in integer form).
+     * @return Whether the member has the modifier.
+     */
+    public static boolean hasModifier(Member member, int modifier) {
+        return hasModifier(member, Modifier.toString(modifier));
+    }
+
+    /**
+     * Gets the modifier string of a member.
+     * @param member The member to get the modifier string from.
+     * @return The modifier string.
+     */
+    public static String getModifierString(Member member) {
+        return Modifier.toString(member.getModifiers());
     }
 
     /**
